158c6eb84b0512ca62f02c6e86a933875a745e1a
Matthew Hartstonge <matt@mykro.co.nz>
2018-09-23 16:55:17 +1200

    :arrow_up: update to support fosite v0.23.0

--- hash_argon2_test.go
+++ hash_argon2_test.go
@@ -1,12 +1,27 @@
 package hasher_test
 
 import (
+	// Standard Library Imports
+	"context"
 	"testing"
 
+	// External Imports
 	"github.com/matthewhartstonge/argon2"
-	"github.com/matthewhartstonge/hasher"
+	"github.com/ory/fosite"
 	"github.com/stretchr/testify/assert"
+
+	// Internal Imports
+	"github.com/matthewhartstonge/hasher"
 )
+
+// TestArgon2_ImplementsHasher ensures that Argon2 implements the fosite.Hasher
+// interface.
+func TestArgon2_ImplementsHasher(t *testing.T) {
+	v := &hasher.Argon2{}
+	var i interface{} = v
+	_, ok := i.(fosite.Hasher)
+	assert.Equal(t, true, ok)
+}
 
 // TestArgon2Hash ensures that a hash is returned from the Argon2 Hasher
 func TestArgon2Hash(t *testing.T) {
@@ -14,25 +29,11 @@ func TestArgon2Hash(t *testing.T) {
 		Config: argon2.DefaultConfig(),
 	}
 	password := []byte("foo")
-	hash, err := h.Hash(password)
+	hash, err := h.Hash(context.Background(), password)
 	assert.Nil(t, err)
 	assert.NotNil(t, hash)
 	assert.NotEqual(t, hash, password)
 }
-
-//// TestArgon2HashLibErr purposely causes the underlying argon2 lib to error to ensure it is reported up the stack
-// todo: reinstate tests once upstream has added in validation errors
-//func TestArgon2HashLibErr(t *testing.T) {
-//	h := &hasher.Argon2{
-//		Config: argon2.DefaultConfig(),
-//	}
-//	h.Config.MemoryCost = 1
-//	password := []byte("foo")
-//	hash, err := h.Hash(password)
-//	assert.Empty(t, hash)
-//	assert.NotNil(t, err)
-//	assert.NotEqual(t, hash, password)
-//}
 
 // TestArgon2CompareEquals ensures a password can be verified successfully when decoded
 func TestArgon2CompareEquals(t *testing.T) {
@@ -40,10 +41,10 @@ func TestArgon2CompareEquals(t *testing.T) {
 		Config: argon2.DefaultConfig(),
 	}
 	password := []byte("foo")
-	hash, err := h.Hash(password)
+	hash, err := h.Hash(context.Background(), password)
 	assert.Nil(t, err)
 	assert.NotNil(t, hash)
-	err = h.Compare(hash, password)
+	err = h.Compare(context.Background(), hash, password)
 	assert.Nil(t, err)
 }
 
@@ -53,24 +54,9 @@ func TestArgon2CompareDifferent(t *testing.T) {
 		Config: argon2.DefaultConfig(),
 	}
 	password := []byte("foo")
-	hash, err := h.Hash(password)
+	hash, err := h.Hash(context.Background(), password)
 	assert.Nil(t, err)
 	assert.NotNil(t, hash)
-	err = h.Compare(hash, []byte("911650fc-df29-4622-8c6f-f43cbacd1ece"))
+	err = h.Compare(context.Background(), hash, []byte("911650fc-df29-4622-8c6f-f43cbacd1ece"))
 	assert.NotNil(t, err)
 }
-
-// TestArgon2HashLibErr purposely causes the underlying argon2 lib to error to ensure it is reported up the stack
-// todo: reinstate tests once upstream has added in validation errors
-//func TestArgon2CompareLibErr(t *testing.T) {
-//	h := &hasher.Argon2{
-//		Config: argon2.DefaultConfig(),
-//	}
-//	h.Config.MemoryCost = 1
-//	password := []byte("foo")
-//	hash, err := h.Hash(password)
-//	assert.Empty(t, hash)
-//	assert.NotNil(t, err)
-//	err = h.Compare(hash, []byte("67e37b8c-e931-41b9-9c01-0c03629a2922"))
-//	assert.NotNil(t, err)
-//}
